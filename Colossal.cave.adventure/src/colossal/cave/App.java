cadd0c73c281fdd39f6ae152843c6306c39f83f6
matthewpagacik12 <64044053+matthewpagacik12@users.noreply.github.com>
2020-09-16 10:44:38 -0400

    Implemented the start of the game

--- Colossal.cave.adventure/src/colossal/cave/App.java
+++ Colossal.cave.adventure/src/colossal/cave/App.java
@@ -9,9 +9,7 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		Player player = new Player();
-		player.playerIntro();
-		
+		System.out.println("Welcome to our Colossal Cave Adventure (Star Wars edition). \nUse HELP command to get all available commands\n" + "In a galaxy far far away you just landed on Naboo....\n");
 		
 			
 		}
--- Colossal.cave.adventure/src/colossal/cave/gameplay/Game.java
+++ Colossal.cave.adventure/src/colossal/cave/gameplay/Game.java
@@ -1,5 +1,18 @@
 package colossal.cave.gameplay;
 
-public class Game {
+import java.util.Scanner;
 
+public class Game {
+	
+	private boolean GameOver;
+	private Scanner scanner;
+	
+	public Game() {
+		this.GameOver = false;
+		this.scanner = new Scanner(System.in);
+	}
+
+	public void GameOver() {
+		this.GameOver = true;
+	}
 }
