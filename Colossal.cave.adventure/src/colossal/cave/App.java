77794ee9ca649772c217858ca9709b3294f3a42b
derek <derek@DESKTOP-BDG9677>
2020-09-19 15:34:18 -0400

    testing

--- Colossal.cave.adventure/src/colossal/cave/App.java
+++ Colossal.cave.adventure/src/colossal/cave/App.java
@@ -8,7 +8,7 @@ public class App {
 
 		System.out.println(
 				"Welcome to our Colossal Cave Adventure (Star Wars edition). \nUse HELP command to get all available commands\n"
-						+ "In a galaxy far far away you just landed on Naboo....\n");
+						+ "In a galaxy far far away you just landed on Nabooy....\n");
 
 		Game g = new Game();
 		g.runGame();
