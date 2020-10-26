6d402e4de115ff7c5e573a365afc71230fc2490a
The1AshPash <69115966+The1AshPash@users.noreply.github.com>
2020-10-25 20:38:35 -0400

    Create Items.java

---
+++ Colossal.cave.adventure/src/colossal/cave/items/Items.java
@@ -0,0 +1,27 @@
+package colossal.cave.items;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Items {
+	
+	private String name;
+	private int score;
+	private int hpChange;
+	private String location;
+	private String flavorText;
+	
+	
+	public Items(String name, int score, int hpChange, String text, String location ){
+		this.name = name;
+		this.score = score;
+		this.hpChange = hpChange;
+		this.flavorText= text;
+		this.location = location;
+		
+	}
+	//trying to upload again
+	
+	
+
+}
