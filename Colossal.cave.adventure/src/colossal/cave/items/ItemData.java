646aefb2e696aba8fd16fc5b6308d78596c8967a
The1AshPash <69115966+The1AshPash@users.noreply.github.com>
2020-10-31 16:47:12 -0400

    Item Data moved

---
+++ Colossal.cave.adventure/src/colossal/cave/items/ItemData.java
@@ -0,0 +1,37 @@
+package colossal.cave.items;
+
+public static class ItemData {
+	private Map<String, Items> item;
+
+    public void init(){
+        initializeItemData();
+
+    }
+
+    public void initializeItemData() {
+		item.put("Medkit", new Items(1, 10, 20, "It's small but it heals well", "TBD"));
+		item.put("Medpack", new Items(2, 40, 100, "A proper Medpack, healing even the most grevious of wounds", "TBD"));
+		item.put("Datapad", new Items(3, 700, 0, "A datapad with information on the Jedi", "TBD"));
+		item.put("Deathsticks", new Items(4, 200, -10, "A taste of death", "TBD"));
+		item.put("Thermaslice", new Items(5, 50, 0, "Yes, this is an easter egg", "TBD"));
+		item.put("Chiss Pyrowall Cracker", new Items(6, 100, 0, "Chiss technology for hacking", "TBD"));
+		item.put("Chiss Comlink Cracker", new Items(7, 100, 0, "Chiss technology for listening into Comlinks", "TBD"));
+		item.put("Incriminating Holograph", new Items(8, 400, 0, "Blackmail material", "TBD"));
+		item.put("VIP Comcode List", new Items(9, 200, 0, "A list of Comcodes of very important people", "TBD"));
+		item.put("Evidence of Jedi passage", new Items(10, 500, 0, "The Jedi were here", "TBD"));
+		item.put("Holograph of Jedi activity", new Items(11, 600, 0, "Holograph of the Jedi in action", "TBD"));
+		item.put("Evidence of Sith passage", new Items(16, 500, 0, "The Sith were here", "TBD"));
+		item.put("Holograph of Sith activity", new Items(17, 600, 0, "Holograph of the Sith in action", "TBD"));
+		item.put("Evidence of fight", new Items(12, 700, 0, "The aftermath of a fight", "TBD"));
+		item.put("Evidence of Count Dooku's Killer", new Items(13, 2000, 0, "The wreckage of battle hide clues to who the perpitrators are", "TBD"));
+		item.put("Evidence of General Grevious's Killer", new Items(14, 2000, 0, "The Saber-burnt remains of the General hide clues to his undoing ", "TBD"));
+		item.put("Evidence of a Sith amongst the Jedi", new Items(15, 2000, 0, "Trechery has a high price", "TBD"));
+		
+		
+		
+		
+	}
+   
+
+
+}
